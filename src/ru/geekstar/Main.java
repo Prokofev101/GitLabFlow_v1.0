0996a20ce742ff897b25c3e987b90cdc1b598aff
Valery Prokofev <vvvpvv1991@gmail.com>
2022-07-02 12:36:38 +0400

    add description main & preproduction

--- src/ru/geekstar/Main.java
+++ src/ru/geekstar/Main.java
@@ -3,6 +3,7 @@ package ru.geekstar;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+	    // main - Ветка предназначена для разработки и может быть не стабильной
+        // preproduction - Ветка для тестирования перед внедрением и является постоянной
     }
 }
